6dbce7ca580e8d35cd1b28738933a55394424fa4
Askar Mulin <askar.mulin@gmail.com>
2017-10-06 20:47:06 +0300

    Changed inner workings of Evo class to use less function calls.

--- Evo.py
+++ Evo.py
@@ -23,40 +23,44 @@ class Evo:
         self.mix_conns = mix_conns
         if not empty: self.fill_with_nets()
 
-    #fitnessFunc MUST call Evo.get_results(fitness) to continue to next net
+    #fitnessFunc MUST return a fitness
     def set_fitness_func(self, func):
         self.fitness_func = func
 
-    # get fitness of current net decided by the fitness_func
-    # and continues to next net
-    def get_results(self, fitness):
-        self.nets[self.curr_net_i].fitness = fitness
+    def evolve(self): #evolve, nonstop
+        for net in self.nets:
+            fitness = self.fitness_func(self,net)
+            self.nets[self.curr_net_i].fitness = fitness
 
-        self.best_record = max(fitness, self.best_record)
-        self.gen_record = max(fitness, self.gen_record)
-        self.next_net()
+            self.best_record = max(fitness, self.best_record)
+            self.gen_record = max(fitness, self.gen_record)
+            self.curr_net_i += 1
 
-    def next_net(self):
-        self.curr_net_i += 1
-        if self.curr_net_i >= len(self.nets):
-            self.next_gen()
+        self.next_gen()
+
+    def evolve_for(self, gen_no):
+        self.gen_limit = gen_no
+        self.evolve()
 
     def next_gen(self):
         self.curr_net_i = 0
         self.current_gen += 1
+
         self.gen_record = 0
         new_nets = []
         self.nets.sort(key=attrgetter("fitness"), reverse=True)
 
-        for ni in range(self.elite_no):
-            new_nets.append(self.nets[ni])
+        if not self.gen_limit or self.gen_limit <= self.current_gen:
+            for ni in range(self.elite_no):
+                new_nets.append(self.nets[ni])
 
-        for ni1 in range(self.elite_no, self.chosen_no+self.elite_no):
-            ni2 = random.randrange(self.elite_no, self.chosen_no+self.elite_no)
-            new_net = self.nets[ni1].reproduce_with(self.nets[ni2], mix_conns=self.mix_conns)
-            new_nets.append(new_net)
+            for ni1 in range(self.elite_no, self.chosen_no+self.elite_no):
+                ni2 = random.randrange(self.elite_no, self.chosen_no+self.elite_no)
+                new_net = self.nets[ni1].reproduce_with(self.nets[ni2], mix_conns=self.mix_conns)
+                new_nets.append(new_net)
 
-        self.fill_with_nets()
+            self.fill_with_nets()
+            self.evolve()
 
     def fill_with_nets(self):
         while len(self.nets) < self.net_no:
--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
-import helpers,generator,json,os
+import helpers, generator, json, os, random
 from Network import Network
-
+from Evo import Evo
 def main():
     pass
+
 if __name__ == "__main__":
     main()
